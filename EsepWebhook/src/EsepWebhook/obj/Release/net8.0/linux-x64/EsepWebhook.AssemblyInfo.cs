d1658fa49d7992e543e0a4189136405c4502757a
dmorsecode <morsecoding@protonmail.com>
2024-11-08 06:30:33 -0500

    Updated parameter for FunctionHandler to object instead of string to fix JSON deserialization errors.

--- EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
+++ EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdde9126dd2a2bbe63510f2086219bb3ff0dfe6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24651bcb2e00bd173d03fdb02f3f319cd3571009")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
